fde847e58c0408debd527bfea3a1d00999811291
Sesilly Cruz <sesilly.cruz@gmail.com>
2020-01-08 09:09:24 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@
 using namespace cimg_library;
 using namespace std;
 const double PI = 3.1415927;
-CImg<unsigned char> image1("ClownFish.bmp");	// Use a bmp image file
+CImg<unsigned char> image1("ClownFish.bmp");		// Use a bmp image file
 CImg<unsigned char> imageFinal = image1;		// imageFinal is set equivalent to image1 because
-												// the program crashes if left uninstantiated
+							// the program crashes if left uninstantiated
 
 struct blur{		// Struct for our threads to use in order to create
 	int width;		// our image
@@ -24,12 +24,12 @@ static void *blurThread(void *threadArgument){
 	struct blur *thisBlur;
 	thisBlur = (struct blur *) threadArgument;
 	float r = thisBlur -> radius;					// Point to the current thread's variables
-	int w = thisBlur -> width;						// and use them to create their section
-	int h = thisBlur -> height;						// of the image
+	int w = thisBlur -> width;					// and use them to create their section
+	int h = thisBlur -> height;					// of the image
 	int rs = (int)ceil(r * 2.57);    				// significant radius
-	for (int i = thisBlur -> i; i < h; i++) { 		// for every column
-		for (int j = thisBlur -> j; j < w; j++) { 	// for every row
-			double									// RGB channels sum, weights sum
+	for (int i = thisBlur -> i; i < h; i++) { 			// for every column
+		for (int j = thisBlur -> j; j < w; j++) { 		// for every row
+			double						// RGB channels sum, weights sum
 			redSum = 0,
 			greenSum = 0,
 			blueSum = 0,
@@ -47,7 +47,7 @@ static void *blurThread(void *threadArgument){
 				}
 			}
 			imageFinal(j, i, 0) = round(redSum / wSum);		// Update the specified coordinates
-			imageFinal(j, i, 1) = round(greenSum / wSum);	// on the final image
+			imageFinal(j, i, 1) = round(greenSum / wSum);		// on the final image
 			imageFinal(j, i, 2) = round(blueSum / wSum);
 		}
 	}
@@ -108,7 +108,7 @@ int main() {		// Using 8 threads to create the blur
 	b8.width = image1.width();
 	b8.radius = 5;
 
-	int start_s = clock();	// Start to calculate time elapsed w/ multi-threading
+	int start_s = clock();					// Start to calculate time elapsed w/ multi-threading
 
 	pthread_create(&t1, NULL, blurThread, (void*)&b1);	// Create all our threads
 	pthread_create(&t2, NULL, blurThread, (void*)&b2);
@@ -120,8 +120,8 @@ int main() {		// Using 8 threads to create the blur
 	pthread_create(&t8, NULL, blurThread, (void*)&b8);
 
 	void *ret;
-	pthread_join(t1, &ret);	// Make the main method wait until all threads are finished
-	pthread_join(t2, &ret);	// so that the final image can be displayed
+	pthread_join(t1, &ret);					// Make the main method wait until all threads are finished
+	pthread_join(t2, &ret);					// so that the final image can be displayed
 	pthread_join(t3, &ret);
 	pthread_join(t4, &ret);
 	pthread_join(t5, &ret);
@@ -129,14 +129,14 @@ int main() {		// Using 8 threads to create the blur
 	pthread_join(t7, &ret);
 	pthread_join(t8, &ret);
 
-	int stop_s = clock();	// All threads are done, so we calculate total elapsed time
+	int stop_s = clock();					// All threads are done, so we calculate total elapsed time
 	cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << endl;
 
 	CImgDisplay main_disp(imageFinal, "GaussianBlur");	// Display the image
 	while (!main_disp.is_closed()) {
 		main_disp.wait();
 	}
-	imageFinal.save("CFBlur8.bmp");		// Save the image
+	imageFinal.save("CFBlur8.bmp");				// Save the image
 
 	pthread_exit(NULL);
 }
